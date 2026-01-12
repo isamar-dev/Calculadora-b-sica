8a3db2b7fe813fca9823b29994adc2d9c799e577
Isamar Dimas <isamar_bcn05@hotmail.com>
2026-01-12 07:43:46 +0100

    Implement basic calculator in Java

---
+++ Calculadora.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Calculadora");
+
+        System.out.print("Introdueix el primer número: ");
+        double x = sc.nextDouble();
+
+        System.out.println("Selecciona una operació (+, -, *, /): ");
+        System.out.print("Operació: ");
+        char operacio = sc.next().charAt(0);
+
+        System.out.print("Ingresa el segundo número: ");
+        double y = sc.nextDouble();
+
+        double resultat;
+
+        switch (operacio) {
+            case '+':
+                resultat = x + y;
+                System.out.println("Resultat: " + resultat);
+                break;
+
+            case '-':
+                resultat = x - y;
+                System.out.println("Resultat: " + resultat);
+                break;
+
+            case '*':
+                resultat = x * y;
+                System.out.println("Resultat: " + resultat);
+                break;
+
+            case '/':
+                if (y != 0) {
+                    resultat = x / y;
+                    System.out.println("Resultat: " + resultat);
+                } else {
+                    System.out.println("Error: no es pot dividir entre 0.");
+                }
+                break;
+
+            default:
+                System.out.println("Operació no vàlida.");
+        }
+
+        sc.close();
+    }
+}
